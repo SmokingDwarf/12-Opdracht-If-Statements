9fae7aec82951725923b383033e19f22d80b13c1
Michael Vlaar <123308669+SmokingDwarf@users.noreply.github.com>
2024-05-18 09:51:27 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,50 @@
+from hidden import * # Dit importeert geheime variabelen
+
+
+# Schrijf de if statements die passen bij de print
+# Voorbeeld:
+# print('var1 is kleiner dan var2')
+var1 = 12
+var2 = 10.8
+if var1 < var2:
+    print('var1 is kleiner dan var2')
+# In dit geval wordt de print dus niet uitgevoerd - dit is prima! 
+
+if a == b:
+  print('a is gelijk aan b')
+
+if c < b:
+  print('c is kleiner dan b')
+
+if d != e:
+  print('d is niet gelijk aan e')
+
+if f > a:
+  print('f is groter dan a')
+
+if 9 < g:
+  print('9 is kleiner dan g')
+
+if c == 5:
+  print('c is gelijk aan 5')
+
+if e > w:
+  print('e groter dan w')
+
+if m < a:
+  print('m kleiner dan a')
+
+if w > m:
+print('w groter dan m')
+
+if a < 10.0:
+ print('a is kleiner dan 10.0')
+
+if 9.5 > d:
+  print('9.5 is groter dan d')
+
+if g > j:
+  print('g groter dan j')
+
+if m < s:
+  print('m kleiner dan s')
